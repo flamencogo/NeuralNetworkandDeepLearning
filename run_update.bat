48866f95ddb2b118d75ec5cf5d390887bdbfc80d
flamencogo <flamencogo@163.com>
2024-06-11 10:24:37 +0800

    update

--- run_update.bat
+++ run_update.bat
@@ -1,6 +1,9 @@
-@REM git config --global user.name "flamencogo"
-@REM git config --global user.email "flamencogo@163.com"
+@REM git config user.name "flamencogo"
+@REM git config user.email "flamencogo@163.com"
 @REM git config user.name
+@REM git config user.email
+@REM git remote rm origin
+@REM git remote add origin git@compaany:flamencogo/NeuralNetworkandDeepLearning.git
 git add .
 git commit -m "update"
 git push origin main
